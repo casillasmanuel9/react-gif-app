f223f1b8950238ac57922bdfe42ce474d8a42174
Juan Manuel Galindo Casillas <casillasmanuel9@gmail.com>
2020-07-23 00:02:02 -0500

    docs agregasdo

---
+++ docs/precache-manifest.31eb2b99520cf4bf52bd595d7c1bf9dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f96678e0166c8c7ebfce80de567a980",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16f9d0ade272f10d45d4",
+    "url": "/static/css/main.90599a34.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16f9d0ade272f10d45d4",
+    "url": "/static/js/main.0e21f062.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
